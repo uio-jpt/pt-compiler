2ee092595909c5b0977df782a45900c590ef1f89
eivindgl <eivindgl@ifi.uio.no>
2010-03-27 21:42:20 +0100

    ast struktur modifisert og resten av programmet oppdatert. bruker collections. har en rar feil. se test/PTExample.java

--- PTFrontend/test/PTExample.java
+++ PTFrontend/test/PTExample.java
@@ -1,8 +1,10 @@
 template TemplateTest1 {
     class T { // rename ok
         double ff;
+
         T() { int vital_code = 0xfeed; }
         T(float o) { }
+        /*        
         T(double d) { }
         class C { double will_be_shadowed; }
         C cmemb_will_be_kept;
@@ -10,25 +12,28 @@ template TemplateTest1 {
         public int minMetode(int x) { return x+99999; }
         public int minMetode(double x) { return 0xface; }
         public static int minMetode(int x) { return x +4569999999; }
+        */
     }
 }
 
 package PackageTest1 {
-    inst TemplateTest1 with T => F;
-    /* adds merge kriterier::
-     * 0. overskrive alt.
-     * 1. bare en tom konstruktur
-     *
-     */
-    class F adds {
-        F(int i) { }
-        F(double ny_d) { }
-        int ff;
-        C cmemb;
-        class C { int override; }
-        public int minMetode(int x) { return x +123; }
-        public static int minMetode(int x) { return x +456; }
-    }
-}
+     inst TemplateTest1 with T => F;
+//     /* adds merge kriterier::
+//      * 0. overskrive alt.
+//      * 1. bare en tom konstruktur
+//      *
+//      */
+     class F adds {
+         /* TODO: denne gir feilmelding. ParameterDecl.getOuterScope() gir ikke treff. vet ikke hvorfor. */         
+//         F(int i) { }
+//         F() { }
+//         C cmemb;
+//         class C { int override; }
+//         public int minMetode(int x) { return x +123; }
+//         public static int minMetode(int x) { return x +456; }
+         int ff;         
+         int kjh;
+     }
+ }
 
 
