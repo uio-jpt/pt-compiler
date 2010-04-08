1c26b4857a1ff2881e098e2d1bb026b998f65d70
Ivar Refsdal <ivarref@luke.ifi.uio.no>
2010-04-08 17:42:39 +0200

    meget enkel merge. *ingen* feilsjekking. (men heller ingen synlige feil..)

--- PTFrontend/test/PTExample.java
+++ PTFrontend/test/PTExample.java
@@ -1,16 +1,39 @@
-template T1 {
-    class C1 {
+template T1 { // PTTemplate (PTDecl)
+    class C1 { // SimpleClass
         double v1;
+        C1 renameThisType;
     }
 }
 
-template T2 {
-    class C2 {
+template T2 { // PTTemplate (PTDecl)
+    class C2 { // SimpleClass
         int v2;
     }
+    class MergeClass {
+        int mergeMe;
+        public MergeClass getMergeClass() { return this; }
+    }
 }
 
-package PackageTest1 {
-    inst T1 with C1 => M;
-    inst T2 with C2 => q;
+package PackageTest1 { //PTPackage (PTDecl)
+    inst T1 with C1 => M; //PTInstDecl
+    //           ^^^^^^^ PTDummyClass
+    //           OrgID=source, ID=target
+    inst T2 with C2 => M, MergeClass => M; //PTInstDecl
+    class M adds {
+        int addsM;
+    }
  }
+/*
+ * med alle 3 separate klasser:
+   renamed AST.ClassDecl [K]
+   renamed AST.TypeAccess [, K]
+
+   renamed AST.ConstructorDecl
+   renamed AST.TypeAccess [, M]
+
+
+ * getClassNamesWithDummyList
+ *
+ *
+ */
