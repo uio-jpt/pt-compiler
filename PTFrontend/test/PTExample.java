8216724de7af7a5c4c3c3f897bd3fb6eb03d5a9e
Eivind Gard Lund <eivindgl@waldo.(none)>
2010-04-08 13:08:57 +0200

    Startet arbeid med merging. samler dummyClass for alle instDecls og kaller nå error() om merging forsøkes.

--- PTFrontend/test/PTExample.java
+++ PTFrontend/test/PTExample.java
@@ -1,42 +1,16 @@
-template TemplateTest1 {
-    class T { // rename ok
-        double ff;
-
-        T() { int vital_code = 0xfeed; }
-        T(float o) { }
-        /*        
-        T(double d) { }
-        class C { double will_be_shadowed; }
-        C cmemb_will_be_kept;
-        public int minMetode() { return 99999; }
-        public int minMetode(int x) { return x+99999; }
-        public int minMetode(double x) { return 0xface; }
-        public static int minMetode(int x) { return x +4569999999; }
-        */
+template T1 {
+    class C1 {
+        double v1;
     }
-    class Q {
-        public T getT() { return null; }
-    }//java.util.LinkedList {}    
+}
+
+template T2 {
+    class C2 {
+        int v2;
+    }
 }
 
 package PackageTest1 {
-    // error if next comment removed. dunno why. haven't looked closely.
-    inst TemplateTest1 with T => F;
-//     adds merge kriterier::
-//     0. overskrive alt.
-//     1. bare en tom konstruktur
-
-
-     class F adds {
-         F(int i) { }
-         F() { }
-         C cmemb;
-         class C { int override; }
-         public int minMetode(int x) { return x +123; }
-         // static og vanlig med lik signatur er heller ikke lov i
-         // vanlig java.
-         // public static int minMetode(int x) { return x +456; }
-         int ff;         
-         int kjh;
-     }
+    inst T1 with C1 => M;
+    inst T2 with C2 => q;
  }
