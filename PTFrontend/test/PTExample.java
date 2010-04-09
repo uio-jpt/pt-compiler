cc5363c1d2c2dac16aa245c6df82f0a8c005c9f1
Ivar Refsdal <ivarref@luke.ifi.uio.no>
2010-04-09 16:06:00 +0200

    sjekking om to klasser kolliderer ved merging

--- PTFrontend/test/PTExample.java
+++ PTFrontend/test/PTExample.java
@@ -1,14 +1,18 @@
 template T1 { // PTTemplate (PTDecl)
-    class X { // SimpleClass
+    class A { // SimpleClass
+        int krasj;
         int x1;
-        X xx1;
+        void f () {
+        }
     }
 }
 
 template T2 { // PTTemplate (PTDecl)
-    class X { // SimpleClass
+    class B { // SimpleClass
+        int krasj;
         int x2;
-        X xx2;
+        void f () {
+        }
     }
 }
 
@@ -19,10 +23,22 @@ package PackageTest1 { //PTPackage (PTDecl)
     //           OrgID=source, ID=target
     inst T2 with C2 => M, MergeClass => M; //PTInstDecl
     */
-    inst T1 with X => Y;
-    inst T2 with X => Y;
+    inst T1 with A => Y;
+    inst T2 with B => Y;
     class Y adds { 
         int fraAddsKlasse; 
+
+        // goal 1
+        // baade A og B har f.. => feilmelding!
+
+        // goal 2
+        void f() {
+            super[A].f()
+            super[B].f()
+        }
+
+        // goal 3
+        // fiks renaming ala A => X (k -> y)
     }
 
  }
