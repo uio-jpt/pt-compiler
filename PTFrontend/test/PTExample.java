891828795808502e8c8a8c1009d80bf7933dd07a
Ivar Refsdal <ivarref@luke.ifi.uio.no>
2010-04-10 15:52:25 +0200

    parsering av super[X](); og super[A].f();, samt generalisering av prettyprint av lister

--- PTFrontend/test/PTExample.java
+++ PTFrontend/test/PTExample.java
@@ -1,46 +1,44 @@
-template TrafficSimulation { // PTTemplate (PTDecl)
-    class Automobile { // SimpleClass
-        int speed;
-        Automobile next;
-    }
-    class Car extends Automobile {
-        int numOfSeats;
-    }
-    class Lorry extends Automobile {
-        int length;
+template T1 { // PTTemplate (PTDecl)
+    class A { // SimpleClass
+        void f () { }
     }
 }
 
-package BusinessTrafficSimulation { //PTPackage (PTDecl)
+template T2 { // PTTemplate (PTDecl)
+    class B { // SimpleClass
+        void f () { }
+    }
+}
+
+package PackageTest1 { //PTPackage (PTDecl)
     /*
     inst T1 with C1 => M; //PTInstDecl
     //           ^^^^^^^ PTDummyClass
     //           OrgID=source, ID=target
     inst T2 with C2 => M, MergeClass => M; //PTInstDecl
     */
-    inst TrafficSimulation with Automobile => Vehicle (next -> nextVehicle), Car => PrivateCar, Lorry => BusinessCar;
-
-    class Vehicle adds {
-        String brand;
-    }
-    class PrivateCar adds {
-        int luggageVolume;
-    }
-    class BusinessCar adds {
-        int loadCapacity;
-    }
+    inst T1 with A => Y;
+    inst T2 with B => Y;
+    class Y adds { 
+        int fraAddsKlasse; 
 
         // goal 1
         // baade A og B har f.. => feilmelding!
 
         // goal 2
-        /*
-        void f() {
-            super[A].f()
-            super[B].f()
+        // dette parserer OK:
+        Y() {
+            super[A]();
         }
-        */
+
+        void f() {
+            super[A].f();
+            super[B].f();
+        }
+
         // goal 3
         // fiks renaming ala A => X (k -> y)
+    }
 
  }
+
