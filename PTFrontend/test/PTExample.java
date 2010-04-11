eb080eaa437912449d3b0b458ba9fb09534e0324
Ivar Refsdal <ivarref@luke.ifi.uio.no>
2010-04-11 15:01:26 +0200

    fiks av renaming rekkefølge.. begynt litt på super[A](); her vil JastAdd alltid overskrive super() kall om man ikke extender en klasse.. dette kan fikses med aa override hostType().isObject()

--- PTFrontend/test/PTExample.java
+++ PTFrontend/test/PTExample.java
@@ -1,44 +1,24 @@
 template T1 { // PTTemplate (PTDecl)
-    class A { // SimpleClass
-        void f () { }
-    }
-}
-
-template T2 { // PTTemplate (PTDecl)
-    class B { // SimpleClass
-        void f () { }
+    class A {
+        /*
+        float localA;
+        A(float y) {
+            super();
+            localA = y;
+        }
+        */
     }
 }
 
 package PackageTest1 { //PTPackage (PTDecl)
-    /*
-    inst T1 with C1 => M; //PTInstDecl
-    //           ^^^^^^^ PTDummyClass
-    //           OrgID=source, ID=target
-    inst T2 with C2 => M, MergeClass => M; //PTInstDecl
-    */
     inst T1 with A => Y;
-    inst T2 with B => Y;
-    class Y adds { 
-        int fraAddsKlasse; 
-
-        // goal 1
-        // baade A og B har f.. => feilmelding!
-
-        // goal 2
-        // dette parserer OK:
-        Y() {
-            super[A]();
+    class C { }
+    class B extends C {
+        B() {
+            super(123.12);
+            //super[A](12.13);
+            //int x = 10;
         }
-
-        void f() {
-            super[A].f();
-            super[B].f();
-        }
-
-        // goal 3
-        // fiks renaming ala A => X (k -> y)
     }
-
  }
 
