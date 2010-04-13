be1cfb80511eff87652e7c1f6549181707e882c0
Eivind Gard Lund <eivindgl@waldo.(none)>
2010-04-13 22:50:07 +0200

    Merging gjøres nå via adds klasser (kan initielt være tomme). gjort refactoring.
    1. Lager tomme add classer for alle dummy nodes uten.
    2. Alle initialiseringer blir lagt til adds klassen.
    3. adds klassen blir så omskrevet til en PTClassDecl.
    
    Har også flytta om på en del kode. mest fordi ting begynte å hope seg opp i InstantiationRewrite.jadd. Håper det er ok for deg (bare flytt rundt på ting som det passer deg).

--- PTFrontend/test/PTExample.java
+++ PTFrontend/test/PTExample.java
@@ -1,5 +1,16 @@
 template T1 { // PTTemplate (PTDecl)
     class A {
+        int x;
+        /*
+        float localA;
+        A(float y) {
+            super();
+            localA = y;
+        }
+        */
+    }
+    class B {
+        int x2;
         /*
         float localA;
         A(float y) {
@@ -11,14 +22,17 @@ template T1 { // PTTemplate (PTDecl)
 }
 
 package PackageTest1 { //PTPackage (PTDecl)
-    inst T1 with A => Y;
-    class C { }
-    class B extends C {
-        B() {
-            super(123.12);
+    inst T1 with A => Y, B => Y;
+    class X { Integer a;}
+    class Z extends X {
+        Z() {
+            //            super(123.12);
             //super[A](12.13);
             //int x = 10;
         }
+    }
+    class Y adds {
+        int ksdjf;
     }
  }
 
