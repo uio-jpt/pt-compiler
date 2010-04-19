23d5409b0605c6cb77a9fc7cda0f292390d371fb
Eivind Gard Lund <eivindgl@waldo.(none)>
2010-04-19 18:19:19 +0200

    adds superklasser. feil og gørr... :(

--- PTFrontend/test/PTExample.java
+++ PTFrontend/test/PTExample.java
@@ -1,27 +1,30 @@
 template T1 { // PTTemplate (PTDecl)
     class A {
         int aLocal;
-        A(int a) {
-            aLocal = a;
-        }
     }
-    class B extends A {
+    class B extends A  {
         String b;
-        B(String bb) {
-            super(123);
-            b = bb;
-        }
+    }
+}
+
+template T2 { // PTTemplate (PTDecl)
+    class C {
+        int zLocal;
+    }
+    class D extends C  {
+        String y;
     }
 }
 
 package PackageTest1 {
-    inst T1 with A => Y, B => Y;
+    inst T1 with A => Z, B => Y;
+    inst T2 with C => Z, D => Y;
     //inst T1 with A => AA, B => BB; // FIX: denne vil knekke ettersom BB ikke subklasser etter rewriting.
     class Y adds {
-        Y() {
-            super[A](123.12);
-            // super[B]("hei"); //TODO: fiks støtte for flere.
-        }
+        int z;
     }
  }
+
+
+
 
