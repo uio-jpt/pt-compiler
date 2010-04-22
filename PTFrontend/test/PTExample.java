3c021f6c6e415f952e35a90e9c9153b94a72ec2c
Eivind Gard Lund <eivindgl@ifi.uio.no>
2010-04-22 18:37:44 +0200

    Rewrites for metodenavn. se RenamedDefinitions. Fortsatt litt usikker på variabelnavn. er det FieldDecl eller FieldDeclaration ?

--- PTFrontend/test/PTExample.java
+++ PTFrontend/test/PTExample.java
@@ -1,28 +1,16 @@
-template T1 { // PTTemplate (PTDecl)
+template T1 {
     class A {
-        int aLocal;
+        int twoTimer() {
+            return 5;
+        }
     }
-    class B extends A  {
-        String b;
-    }
-}
-
-template T2 { // PTTemplate (PTDecl)
-    class C {
-        int zLocal;
-    }
-    class D extends C  {
-        String y;
+    class B  {
+        int twoTimer;
     }
 }
 
 package PackageTest1 {
-    inst T1 with A => Z, B => Y;
-    inst T2 with C => Z, D => Y;
-    //inst T1 with A => AA, B => BB; // FIX: denne vil knekke ettersom BB ikke subklasser etter rewriting.
-    class Y adds {
-        int z;
-    }
+    inst T1 with A => Z(twoTimer -> b), B => Z ;
  }
 
 
