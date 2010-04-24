e9a920222408c2407494de72d7da47ee0b92a050
Ivar Refsdal <ivarref@student.uio.no>
2010-04-24 22:29:45 +0200

    generell parsering av super[A]() og super[A].x()

--- PTFrontend/test/PTExample.java
+++ PTFrontend/test/PTExample.java
@@ -1,18 +1,23 @@
-template T1 {
+template T1 { // PTTemplate (PTDecl)
     class A {
-        int twoTimer() {
-            return 5;
-        }
+        int f() { return 3; }
     }
-    class B  {
-        int twoTimer;
+    class B {
+        int f() { return 1; }
     }
 }
 
 package PackageTest1 {
-    inst T1 with A => Z(twoTimer -> b), B => Z(twoTimer -> x) ;
+    inst T1 with B => Z, A => Z;
+    class Z adds {
+        Z() {
+            super[A]("hei");
+            super[B]("hei");
+            super[A].f("hei");
+            super[B].g("hei");
+        }
+        int f() {
+            return 123;
+        }
+    }
  }
-
-
-
-
