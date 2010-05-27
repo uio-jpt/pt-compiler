34416f118e0253c38eab447d7b37ddd8781805b5
Eivind Gard Lund <eivindgl@ifi.uio.no>
2010-05-27 18:30:52 +0200

    fix: no duplicate imports on merge fix equal template/package name within compilation unit

---
+++ PTFrontend/test/single_file/basic/SameNameForPackageAndTemplate_fail.java
@@ -0,0 +1,18 @@
+// jeg tror dette bør være lov. egl
+
+template Graph {
+    class Node {
+        // some code
+        int x;
+    }
+
+    class Edge {
+        // more code
+        int x;
+    }
+}
+
+package Graph {
+    inst Graph;
+}
+
