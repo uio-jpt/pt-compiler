c7feec57da9b1d88b890d6548ab28941644b8b17
unknown <eivind@.(none)>
2010-06-08 07:59:44 -0700

    works mostly on windows (ant testcompile breaks), but that's a minor issue.

--- PTFrontend/src/testutils/utils/GenericBuildXML.java
+++ PTFrontend/src/testutils/utils/GenericBuildXML.java
@@ -2,14 +2,15 @@ package testutils.utils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.security.CodeSource;
 import java.util.Scanner;
 import java.util.jar.JarFile;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipException;
 
 import org.apache.commons.io.IOUtils;
 
@@ -18,50 +19,65 @@ import testutils.exceptions.FatalErrorException;
 
 public class GenericBuildXML {
 
-	private static final String GENERIC_BUILD_XML_LOCATION = "txt" + File.separator + "generic-build.xml";
+	private static final String GENERIC_BUILD_XML_LOCATION = "txt/generic-build.xml";
 
 	private static String readGenericBuildFile(File jarPath) {
-		StringWriter writer = new StringWriter();
 		try {
+			StringWriter writer = new StringWriter();
 			JarFile jar = new JarFile(jarPath);
 			ZipEntry entry = jar.getEntry(GENERIC_BUILD_XML_LOCATION);
 			InputStream stream = jar.getInputStream(entry);
 			IOUtils.copy(stream, writer);
-		} catch (ZipException e) {
-			// happens when run from classfiles and not jar file
-			Scanner f;
-			try {
-				f = new Scanner(new File("resources/txt/generic-build.xml"));
-			} catch (FileNotFoundException e1) {
-				f = null;
-				throw new FatalErrorException("generic-build.xml not found.");
-			}
-			StringBuffer sb = new StringBuffer();
-			while (f.hasNextLine()) {
-				sb.append(f.nextLine() + "\n");
-			}
-			return sb.toString();
-		} catch (IOException e) {
+			return writer.toString();
+		} catch (Exception e) {
 			throw new FatalErrorException(String.format(
-					"Couldn't read file %s from jarfile at path: %s",
-					GENERIC_BUILD_XML_LOCATION, jarPath));
+					"Couldn't read file %s from jarfile",
+					GENERIC_BUILD_XML_LOCATION));
 		}
-		return writer.toString();
 	}
 
 	private static File getJarPath() {
 		CodeSource codeSource = PTToJavaPackage.class.getProtectionDomain()
 				.getCodeSource();
-		String jarPath = codeSource.getLocation().getPath();
-		return new File(jarPath);
+
+		URL url = codeSource.getLocation();
+		URI uri;
+		try {
+			uri = new URI(url.getPath());
+			return new File(uri.getPath());
+		} catch (URISyntaxException e) {
+			throw new FatalErrorException("unable to locate JAR file");
+		}
 	}
 
 	public static String generateBuildFile(String outputFolderName) {
-		File jarPath = getJarPath();
-		String genericBuildFile = readGenericBuildFile(jarPath);
+		String genericBuildFile;
+		try {
+			File jarPath = getJarPath();
+			genericBuildFile = readGenericBuildFile(jarPath);
+		} catch (FatalErrorException e) {
+			// happens when run directly from class files instead of jar file
+			genericBuildFile = readBuildfileFromFileSystem();
+		}
 		String buildFileText = genericBuildFile.replaceFirst("PROJECTNAME",
 				outputFolderName);
 		return buildFileText;
 	}
 
+	private static String readBuildfileFromFileSystem() {
+		Scanner scanner;
+		File file = new File("resources/txt/generic-build.xml");
+		try {
+			scanner = new Scanner(file);
+			StringBuffer sb = new StringBuffer();
+			while (scanner.hasNextLine()) {
+				sb.append(scanner.nextLine() + "\n");
+			}
+			return sb.toString();
+		} catch (FileNotFoundException e) {
+			throw new FatalErrorException(
+					"Unable to locate jar file and failed to read generic-build.xml from the filesystem");
+		}
+
+	}
 }
