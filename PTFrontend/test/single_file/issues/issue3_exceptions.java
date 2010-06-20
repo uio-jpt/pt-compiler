dc1e954496fe2bc6dc7bbb08535a1698a2b6c3a3
Ivar Refsdal <ivarref@student.uio.no>
2010-06-20 15:59:03 +0200

    update issue3...

--- PTFrontend/test/single_file/issues/issue3_exceptions.java
+++ PTFrontend/test/single_file/issues/issue3_exceptions.java
@@ -3,12 +3,13 @@
  ant testsingle -Dname=test/single_file/issues/issue3_exceptions.java
 
  Det var ikke exception som feiler, men bruk av System.out (!).
+ Status: not fixed.
  */
 
 template TestExceptions {
     class Foo  {
         public void bar() {
-            throw new RuntimeException("Hello");
+            System.out.println("hello");
         }
     }
 }
