29cbcd1d8029678144eff3aadc59c703e2800bfa
Ivar Refsdal <ivarref@student.uio.no>
2010-06-26 16:23:26 +0200

    patched issue 3..

--- PTFrontend/test/single_file/issues/issue3_exceptions.java
+++ PTFrontend/test/single_file/issues/issue3_exceptions.java
@@ -2,8 +2,9 @@
  http://github.com/eivindgl/pt-compiler/issues#issue/3
  ant testsingle -Dname=test/single_file/issues/issue3_exceptions.java
 
+
  Det var ikke exception som feiler, men bruk av System.out (!).
- Status: not fixed.
+ Status: Fixed.
  */
 
 template TestExceptions {
