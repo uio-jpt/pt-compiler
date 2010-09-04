1f9c0953477b37cc6d1fa3e79378d4f676796db8
Ivar Refsdal <ivarref@ben.ifi.uio.no>
2010-09-04 18:11:19 +0200

    buggy, men commit uansett..

--- PTFrontend/src/testutils/src/errorcheck/ErrorCheckBeforeRewrite.java
+++ PTFrontend/src/testutils/src/errorcheck/ErrorCheckBeforeRewrite.java
@@ -1,16 +1,17 @@
 package errorcheck;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
 import AST.ClassDecl;
 import AST.CompilationUnit;
 import AST.PTDecl;
+import AST.PTDummyClass;
 import AST.PTInstDecl;
 import AST.PTPackage;
 import AST.SimpleClass;
 import AST.TemplateMethodAccess;
-import AST.TypeAccess;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
@@ -41,34 +42,43 @@ public class ErrorCheckBeforeRewrite {
         };
         v.searchDown(unit, v, "find", false);
         
-        v = new VisitNodes() {
-        	List<String> constructorNames = Lists.newLinkedList();
-        	Set<String> visited = Sets.newHashSet();
-        	        	
-        	public void find(PTPackage p) {
-        		searchDown(p, this, "handleClass", false);
-        	}
-        	
-        	public void recurseInstantiations(PTDummyClass p) {
-        	}
-        	
-        	public void handleClass(SimpleClass p) {
-            	constructorNames = Lists.newLinkedList();
-            	visited = Sets.newHashSet();
-            	
-            	String topSuperClassName = p.getTopSupername();
-            	String key = p.getPTDecl().getID() + "::" + topSuperClassName;
-            	
-            	if (!visited.contains(key)) {
-            		visited.add(key);
-            		constructorNames.add(p.getPTDecl().getID() + "::" + p.getClassDecl().getID());
-            	}
-            	
-            	PTDecl scope = (PTDecl) p.getParentClass(PTDecl.class);
-            	searchDown(scope, this, "recurseInstantiations", false);
-            	
-        	}
-        };
+//        v = new VisitNodes() {
+//        	List<String> constructorNames = Lists.newLinkedList();
+//        	Set<String> visited = Sets.newHashSet();
+//        	        	
+//        	public void find(PTPackage p) {
+//        		searchDown(p, this, "handleClass", false);
+//        	}
+//        	
+//        	public void recurseInstantiations(PTDummyClass p, String fromClass) {
+//        		//System.out.println("Looking at " + p + " with " + fromClass);
+//        		if (p.getID().equals(fromClass)) {
+//        			PTInstDecl inst = (PTInstDecl)p.getParentClass(PTInstDecl.class);
+//        		
+//        			//getOriginator(p.getParentClass(CompilationUnit.class), inst.getID(), p.getOrgID());
+//        			
+//        			//SimpleClass s = (SimpleClass)p.getOriginator().getParentClass(SimpleClass.class);
+//        			//if (s!=null) handleClass(s);
+//        		}
+//        	}
+//        	
+//        	public void handleClass(SimpleClass p) {
+//            	constructorNames = Lists.newLinkedList();
+//            	visited = Sets.newHashSet();
+//            	
+//            	System.out.println("Looking at class " + p.getID());
+//            	String topSuperClassName = p.getTopMostSuperName();
+//            	String key = p.getPTDecl().getID() + "::" + "hmm";
+//            	
+//            	if (!visited.contains(key)) {
+//            		visited.add(key);
+//            		constructorNames.add(p.getPTDecl().getID() + "::" + p.getClassDecl().getID());
+//            	}
+//            	
+//            	PTDecl scope = (PTDecl) p.getParentClass(PTDecl.class);
+//            	searchDown(scope, this, "recurseInstantiations", false, p.getID());
+//        	}
+//        };
         
         // I hver simpleclass node:
         
@@ -79,7 +89,32 @@ public class ErrorCheckBeforeRewrite {
          * expand instantiations
          * move up to super
          */
+        //v.searchDown(unit, v, "find", false);
         
-        v.searchDown(unit, v, "find", false);
+        final HashMap<String, ClassDecl> globalScope = new HashMap<String, ClassDecl>();
+        //final HashMap<String, String> instantiations = new HashMap<String, String>();
+        final HashMap<String, String> instantiationsReverse = new HashMap<String, String>();
+        
+        VisitNodes classHierarchy = new VisitNodes() {
+        	public void find(ClassDecl cd) {
+        		PTDecl scope = (PTDecl)cd.getParentClass(PTDecl.class);
+        		String key = scope.getID() + "::" + cd.getID();
+        		globalScope.put(key, cd);
+        		System.out.println("class " + key + " extends " + cd.getSuperClassName());
+        	}
+        	
+        	public void find(PTDummyClass instTuple) {
+        		PTDecl scope = (PTDecl)instTuple.getParentClass(PTDecl.class);
+        		PTInstDecl importedFromScope = (PTInstDecl)instTuple.getParentClass(PTInstDecl.class);
+        		String fromKey = importedFromScope.getID() + "::" + instTuple.getOrgID();
+        		String toKey = scope.getID() + "::" + instTuple.getID();
+        		System.out.println("inst " + fromKey + " => " + toKey);
+        		//instantiations.put(fromKey, toKey);
+        		instantiationsReverse.put(toKey, fromKey);
+        	}
+        };
+        
+        classHierarchy.searchDown(unit, classHierarchy, "find", false);
+        
 	}
 }
--- PTFrontend/src/testutils/src/errorcheck/VisitNodes.java
+++ PTFrontend/src/testutils/src/errorcheck/VisitNodes.java
@@ -7,12 +7,24 @@ import AST.ASTNode;
 
 public class VisitNodes {
 	public boolean searchDown(ASTNode startPoint, Object targetForInvocation, String methodName, boolean singleMatch) {
+		return searchDown(startPoint, targetForInvocation, methodName, singleMatch, null);
+	}
+
+	public boolean searchDown(ASTNode startPoint, Object targetForInvocation, String methodName, boolean singleMatch, Object extraArgument) {
         boolean match = false;
         Class methodParamClass = startPoint.getClass();
         while (methodParamClass != null) {
             try {
-                Method m = targetForInvocation.getClass().getMethod(methodName, methodParamClass);
-                m.invoke(targetForInvocation.getClass().cast(targetForInvocation), startPoint);
+            	Method m;
+            	if (extraArgument==null) {
+                    m = targetForInvocation.getClass().getMethod(methodName, methodParamClass);            		
+                	m.invoke(targetForInvocation.getClass().cast(targetForInvocation), startPoint);
+            	}
+            	else {
+            		m = targetForInvocation.getClass().getMethod(methodName, methodParamClass, extraArgument.getClass());
+                	m.invoke(targetForInvocation.getClass().cast(targetForInvocation), startPoint, extraArgument);
+            	}
+
                 //System.out.println( "searchDown for : '" + methodName + "' of "  + this.getClass() + " => OK");
                 match = true;
                 break;
@@ -25,14 +37,15 @@ public class VisitNodes {
             } catch (IllegalAccessException e) {
                 e.printStackTrace();
             } catch (InvocationTargetException e) {
-                e.printStackTrace();
+                System.err.println("Methodname = " + methodName + " object " + startPoint);
+            	e.printStackTrace();
             }
             methodParamClass = methodParamClass.getSuperclass();
         }
         if (match && singleMatch) return true;
 
         for (int i=0; i<startPoint.getNumChildNoTransform(); i++) {
-            match = searchDown(startPoint.getChildNoTransform(i), targetForInvocation, methodName, singleMatch);
+            match = searchDown(startPoint.getChildNoTransform(i), targetForInvocation, methodName, singleMatch, extraArgument);
             if (match && singleMatch) return true;
         }
 
