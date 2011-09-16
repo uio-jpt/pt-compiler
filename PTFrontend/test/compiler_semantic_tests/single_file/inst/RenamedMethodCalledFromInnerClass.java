8e14494c7806a929af9004c91f77ff625e5c1525
Steinar Vittersø Kaldager <steinarvk@gmail.com>
2011-09-16 04:09:27 +0200

    flere tester
    Disse demonstrerer at det nye rename-systemet for metoder
    dekker noen tilfeller som det gamle ikke håndterte.

---
+++ PTFrontend/test/compiler_semantic_tests/single_file/inst/RenamedMethodCalledFromInnerClass.java
@@ -0,0 +1,21 @@
+template T {
+    class X {
+        private class Y {
+            public void callPrintMessage() {
+                new X().printMessage();
+            }
+        }
+
+        public void printMessage() {
+            System.out.println( "Hello world!" );
+        }
+
+        public void indirectPrintMessage() {
+            new Y().callPrintMessage();
+        }
+    }
+}
+
+package P {
+    inst T with X => X ( printMessage() -> f );
+}
