8ffaa5082216e62e62f165472cde8f4bb968f7e0
Kenneth Solbø Andersen <ksa618@gmail.com>
2013-03-02 12:49:30 +0100

    Add some runtime tests
    These are tests for bugs #21 and #22

---
+++ PTFrontend/test/runtime_tests/tsuper_in_array_name/src/Test.java
@@ -0,0 +1,9 @@
+package P {
+    class Test {
+        public static void main(String[] args) {
+            int[] namewithtsuper = new int[1];
+            namewithtsuper[0] = 42;
+            System.out.println(namewithtsuper[0]);
+        }
+    }
+}
